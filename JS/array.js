ca4de93d535d939dddf69129d21c39b7b204bbc2
cpsoucy <96217410+cpsoucy@users.noreply.github.com>
2023-03-04 20:03:08 -0500

    added js

---
+++ JS/array.js
@@ -0,0 +1,22 @@
+const quotesArr = [
+    {
+      "text": "History doesn't repeat itself, but it does rhyme.",
+      "author": "Mark Twain",
+      "category": "old"
+    },
+    {
+      "text": "It's not in the dreaming, it's in the doing.",
+      "author": "Mark Cuban",
+      "category": "modern"
+    },
+    {
+        "text": "Time is long but life is short.",
+        "author": "Stevie Wonder",
+        "category": "short"
+    },
+    {
+        "text": "Time is too slow for those who wait, too swift for those who fear, too long for those who grieve, too short for those who rejoice, but for those who love, time is eternity.",
+        "author": "Henry van Dyke",
+        "category": "long"
+    }
+]
