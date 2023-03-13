dbe62d7109e4bf89160a8fb79159ec12b0cf3432
cpsoucy <96217410+cpsoucy@users.noreply.github.com>
2023-03-13 18:28:56 -0400

    made fixes to apply simple.css, fixed JS issue

--- JS/array.js
+++ JS/array.js
@@ -5,8 +5,18 @@ const quotesArr = [
       "category": "old"
     },
     {
+      "text": "The farther backward you can look, the farther forward you are likely to see.",
+      "author": "Winston S. Churchill",
+      "category": "old"
+    },
+    {
       "text": "It's not in the dreaming, it's in the doing.",
       "author": "Mark Cuban",
+      "category": "modern"
+    },
+    {
+      "text": "There’s something about arriving in new cities, wandering empty streets with no destination. I will never lose the love for the arriving, but I'm born to leave.",
+      "author": "Charlotte Eriksson",
       "category": "modern"
     },
     {
@@ -15,8 +25,18 @@ const quotesArr = [
         "category": "short"
     },
     {
+      "text": "Come tomorrow, I'll wake up new.",
+      "author": "Chad Sugg",
+      "category": "short"
+    },
+    {
         "text": "Time is too slow for those who wait, too swift for those who fear, too long for those who grieve, too short for those who rejoice, but for those who love, time is eternity.",
         "author": "Henry van Dyke",
         "category": "long"
-    }
+    },
+    {
+      "text": "Everyone has that moment I think, the moment when something so momentous happens that it rips your very being into small pieces. And then you have to stop. For a long time, you gather your pieces. And it takes such a very long time, not to fit them back together, but to assemble them in a new way, not necessarily a better way. More, a way you can live with until you know for certain that this piece should go there, and that one there.",
+      "author": "Kathleen Glasgow",
+      "category": "long"
+  }
 ]
--- JS/page.js
+++ JS/page.js
@@ -31,3 +31,8 @@ function getCat() {
       }
     });
   }
+
+  function codeAddress() {
+    getCat();
+}
+window.onload = codeAddress;
